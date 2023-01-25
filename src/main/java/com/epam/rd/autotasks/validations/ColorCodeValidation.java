317ab8b5934e094baa1885f9100701e0e735b527
Zapletin Daniil <Zaplot1994@gmail.com>
2023-01-25 20:05:40 +0300

    Task done with all tests passed

--- src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
+++ src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
@@ -1,13 +1,13 @@
 package com.epam.rd.autotasks.validations;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ColorCodeValidation {
     public static boolean validateColorCode(String color) {
-
-        // Put your code here
-
+        if (color == null ){ return false;}
+        String regex = "^#([0-9a-fA-F]{3}){1,2}$";
+        Pattern pattern = Pattern.compile(regex);
+        return pattern.matcher(color).matches();
     }
 }
 
